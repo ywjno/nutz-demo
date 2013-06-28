fbc2e9a5d8b6943b3b5e6a2b532cbe958a4af6a2
ywjno <ywjno.dev@gmail.com>
2013-06-28 15:41:33 +0800

    修改ApplicationController类

--- src/app/controllers/ApplicationController.java
+++ src/app/controllers/ApplicationController.java
@@ -1,5 +1,13 @@
 package app.controllers;
 
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
+
 public class ApplicationController {
 
+    @At("/home/index")
+    @Ok("raw")
+    public static String index() {
+        return "Hello Nutz.";
+    }
 }
