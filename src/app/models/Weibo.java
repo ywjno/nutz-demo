8eb1879752d665b4e2f3494d6640b555d0aafb79
ywjno <ywjno.dev@gmail.com>
2013-07-01 14:18:47 +0800

    生成weibo的pojo

---
+++ src/app/models/Weibo.java
@@ -0,0 +1,55 @@
+package app.models;
+
+import java.sql.Timestamp;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table(value = "weibos")
+public class Weibo {
+
+    @Id
+    private int id;
+
+    @Column
+    private String content;
+
+    @Column("created_at")
+    private Timestamp createdAt;
+
+    @Column("updated_at")
+    private Timestamp updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
