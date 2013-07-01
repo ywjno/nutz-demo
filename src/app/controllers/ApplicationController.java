162a6a648a9d945a91da489f8e0f5fd1d4cb9285
ywjno <ywjno.dev@gmail.com>
2013-07-01 17:07:26 +0800

    生成WeiboController子模块类

--- src/app/controllers/ApplicationController.java
+++ src/app/controllers/ApplicationController.java
@@ -1,8 +1,16 @@
 package app.controllers;
 
 import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.IocBy;
+import org.nutz.mvc.annotation.Modules;
 import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.ioc.provider.ComboIocProvider;
 
+@IocBy(type = ComboIocProvider.class, args = {"*org.nutz.ioc.loader.json.JsonLoader",
+                                              "dao.js",
+                                              "*org.nutz.ioc.loader.annotation.AnnotationIocLoader",
+                                              "app"})
+@Modules({WeiboController.class})
 public class ApplicationController {
 
     @At("/home/index")
---
+++ src/app/controllers/WeiboController.java
@@ -0,0 +1,23 @@
+package app.controllers;
+
+import java.util.List;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.GET;
+import org.nutz.mvc.annotation.Ok;
+import org.nutz.service.IdEntityService;
+
+import app.models.Weibo;
+
+@IocBean(fields = "dao")
+@At("/weibo")
+public class WeiboController extends IdEntityService<Weibo> {
+
+    @At
+    @GET
+    @Ok("jsp:jsp.weibo.index")
+    public List<?> index() {
+        return dao().query(Weibo.class, null);
+    }
+}
