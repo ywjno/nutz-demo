eb573d8aa4790ae3c2c7070c3b5c40e40166b058
ywjno <ywjno.dev@gmail.com>
2013-07-03 14:06:29 +0800

    生成comment的pojo类

---
+++ src/app/models/Comment.java
@@ -0,0 +1,66 @@
+package app.models;
+
+import java.sql.Timestamp;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table(value = "comments")
+public class Comment {
+
+    @Id
+    private int id;
+
+    @Column
+    private String content;
+
+    @Column("weibo_id")
+    private int weiboId;
+
+    @Column("created_at")
+    private Timestamp createdAt;
+
+    @Column("updated_at")
+    private Timestamp updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getWeiboId() {
+        return weiboId;
+    }
+
+    public void setWeiboId(int weiboId) {
+        this.weiboId = weiboId;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
